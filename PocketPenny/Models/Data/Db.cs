499232d59f7039046befbc7823ef6c35246e9b12
admin <richard.bota@khk.ee>
2017-03-11 21:47:37 +0200

    Created Shop view model and database table for Categories

--- PocketPenny/Models/Data/Db.cs
+++ PocketPenny/Models/Data/Db.cs
@@ -10,5 +10,6 @@ namespace PocketPenny.Models.Data
     {
         public DbSet<PageDTO> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
+        public DbSet<CategoryDTO> Categories { get; set; }
     }
 }
