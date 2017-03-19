11becebd1820bfafe4a988cfea8bb948a063a8a9
admin <richard.bota@khk.ee>
2017-03-19 23:30:51 +0200

    Admin page edit product delete functionality in deopzone

--- PocketPenny/Areas/Admin/Controllers/ShopController.cs
+++ PocketPenny/Areas/Admin/Controllers/ShopController.cs
@@ -345,7 +345,7 @@ namespace PocketPenny.Areas.Admin.Controllers
             return View(model);
         }
 
-        // GET: Admin/Shop/EditProduct/id
+        // POST: Admin/Shop/EditProduct/id
         [HttpPost]
         public ActionResult EditProduct(ProductVM model, HttpPostedFileBase file)
         {
@@ -483,6 +483,56 @@ namespace PocketPenny.Areas.Admin.Controllers
 
             // Redirect 
             return RedirectToAction("Products");
+        }
+
+        // POST: Admin/Shop/SaveGalleryImages
+        [HttpPost]
+        public void SaveGalleryImages(int id)
+        {
+            // Loop through files
+            foreach (string fileName in Request.Files)
+            {
+                // Init the file
+                HttpPostedFileBase file = Request.Files[fileName];
+
+                // Check it's not null
+                if ( file != null && file.ContentLength > 0)
+                {
+
+                    // Set directory paths
+                    var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\Uploads", Server.MapPath(@"\")));
+
+                    string pathString1 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery");
+                    string pathString2 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery\\Thumbs");
+
+                    // Set image paths
+                    var path = string.Format("{0}\\{1}", pathString1, file.FileName);
+                    var path2 = string.Format("{0}\\{1}", pathString2, file.FileName);
+
+                    // Save original and thumb
+
+                    file.SaveAs(path);
+                    WebImage img = new WebImage(file.InputStream);
+                    img.Resize(200, 200);
+                    img.Save(path2);
+                }
+
+            }
+
+        }
+
+        // POST: Admin/Shop/DeleteImage
+        [HttpPost]
+        public void DeleteImage(int id, string imageName)
+        {
+            string fullPath1 = Request.MapPath("~/Images/Uploads/Products/" + id.ToString() + "/Gallery/" + imageName);
+            string fullPath2 = Request.MapPath("~/Images/Uploads/Products/" + id.ToString() + "/Gallery/Thumbs/" + imageName);
+
+            if (System.IO.File.Exists(fullPath1))
+                System.IO.File.Delete(fullPath1);
+
+            if (System.IO.File.Exists(fullPath2))
+                System.IO.File.Delete(fullPath2);
         }
     }
 }
