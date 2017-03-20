7966c9a56047cc406667d98ca013f322633ede87
admin <richard.bota@khk.ee>
2017-03-21 00:01:37 +0200

    Added Cart controller and view

--- PocketPenny/App_Start/RouteConfig.cs
+++ PocketPenny/App_Start/RouteConfig.cs
@@ -13,7 +13,10 @@ namespace PocketPenny
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             
+            routes.MapRoute("Cart", "Cart/{action}/{id}", new { controller = "Cart", action = "Index", id = UrlParameter.Optional }, new[] { "PocketPenny.Controllers" });
+
             routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "PocketPenny.Controllers" });
+
             routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "PocketPenny.Controllers" });
             routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "PocketPenny.Controllers" });
             routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index"}, new [] { "PocketPenny.Controllers" });
---
+++ PocketPenny/Controllers/CartController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PocketPenny.Models.ViewModels.Cart;
+
+namespace PocketPenny.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CartPartial()
+        {
+            // Init CartVM
+            CartVM model = new CartVM();
+
+            // Init quantity
+            int gty = 0;
+
+            // Init price
+            decimal price = 0m;
+
+            // Check for cart session
+            if (Session["cart"] != null)
+            {                
+                // Get total gty and price to 0
+                var list = (List<CartVM>) Session["cart"];
+
+                foreach (var item in list)
+                {
+                    gty += item.Quantity;
+                    price += item.Quantity * item.Price;
+                }
+            }
+            else
+            {
+                // Or set gty and price to 0
+                model.Quantity = 0;
+                model.Price = 0m;
+            }
+
+            // Return partial view with model 
+            return PartialView(model);
+        }
+    }
+}
