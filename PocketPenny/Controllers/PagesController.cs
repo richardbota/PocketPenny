892320093e655309434574d2dd7dbc6359506578
admin <richard.bota@khk.ee>
2017-03-20 19:22:52 +0200

    Created pages view and controller

---
+++ PocketPenny/Controllers/PagesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PocketPenny.Models.Data;
+using PocketPenny.Models.ViewModels.Pages;
+
+namespace PocketPenny.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{page}
+        public ActionResult Index(string page = "")
+        {
+            // Get/set page slug
+            if (page == "")
+                page = "home";
+
+            // Declare model and DTO
+            PageVM model;
+            PageDTO dto;
+
+            // Check if page exists
+            using (Db db = new Db())
+            {
+                if (!db.Pages.Any(x => x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index", new {page = ""});
+                }
+            }
+            // Get page DTO
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            // Set page title
+            ViewBag.PageTitle = dto.Title;
+
+            // Check for sidebar
+            if (dto.HasSidebar == true)
+            {
+                ViewBag.Sidebar = "Yes";
+            }
+            else
+            {
+                ViewBag.Sidebar = "No";
+            }
+
+            // Init model
+            model =  new PageVM(dto);
+
+            // Return view with model
+            return View(model);
+        }
+    }
+}
