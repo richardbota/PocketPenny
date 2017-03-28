98a89dd21ab8bb2816649f84ae58a602bfa6d7e2
admin <richard.bota@khk.ee>
2017-03-28 23:17:45 +0300

    Added db tables(user and role tables), created user registration form

--- PocketPenny/App_Start/RouteConfig.cs
+++ PocketPenny/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace PocketPenny
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             
+            routes.MapRoute("Account", "Account/{action}/{id}", new { controller = "Account", action = "Index", id = UrlParameter.Optional }, new[] { "PocketPenny.Controllers" });
+
             routes.MapRoute("Cart", "Cart/{action}/{id}", new { controller = "Cart", action = "Index", id = UrlParameter.Optional }, new[] { "PocketPenny.Controllers" });
 
             routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "PocketPenny.Controllers" });
