1ec83370025c2ddf820c6b8afbeeeceaaaddc08a
admin <richard.bota@khk.ee>
2017-04-01 11:30:07 +0300

    Log in and log out functionality

--- PocketPenny/Controllers/AccountController.cs
+++ PocketPenny/Controllers/AccountController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using PocketPenny.Models.ViewModels.Account;
+using PocketPenny.Models.Data;
 
 namespace PocketPenny.Controllers
 {
@@ -15,6 +18,7 @@ namespace PocketPenny.Controllers
         }
 
         // GET: /account/login
+        [HttpGet]
         public ActionResult Login()
         {
             // Confirm user is not logeed in
@@ -29,11 +33,116 @@ namespace PocketPenny.Controllers
             return View();
         }
 
+        // POST: /account/login
+        [HttpPost]
+        public ActionResult Login(LoginUserVM model)
+        {
+            // Check model state
+            if (! ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Check if the user is valid
+            bool isValid = false;
+
+            using (Db db = new Db())
+            {
+                if (db.Users.Any(x => x.Username.Equals(model.Username) && x.Password.Equals(model.Password)))
+                {
+                    isValid = true;
+                }
+            }
+
+            if (!isValid)
+            {
+                ModelState.AddModelError("", "Invalid username or password.");
+                return View(model);
+            }
+            else
+            {
+                FormsAuthentication.SetAuthCookie(model.Username, model.RememberMe);
+                return Redirect(FormsAuthentication.GetRedirectUrl(model.Username, model.RememberMe));
+            }
+        }
+
         // GET: /account/create-account
         [ActionName("create-account")]
+        [HttpGet]
         public ActionResult CreateAccount()
         {
             return View("CreateAccount");
+        }
+
+        // POST: /account/create-account
+        [ActionName("create-account")]
+        [HttpPost]
+        public ActionResult CreateAccount(UserVM model)
+        {
+            // Check model state
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAccount", model);
+            }
+
+            // Check if password match
+            if (!model.Password.Equals(model.ConfirmPassword))
+            {
+                ModelState.AddModelError("", "Password do not match.");
+                return View("CreateAccount", model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Make sure username is unique
+                if (db.Users.Any(x => x.Username.Equals(model.Username)))
+                {
+                    ModelState.AddModelError("", "Username " + model.Username + " is taken.");
+                    model.Username = "";
+                    return View("CreateAccount", model);
+                }
+
+                // Create userDTO
+                UserDTO userDTO = new UserDTO()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    Username = model.Username,
+                    Password = model.Password
+                };
+
+                // Add the DTO
+                db.Users.Add(userDTO);
+
+                // Save
+                db.SaveChanges();
+
+                // Add to UserRolesDTO
+                int id = userDTO.Id;
+
+                UserRoleDTO userRolesDTO = new UserRoleDTO()
+                {
+                    UserId = id,
+                    RoleId = 2
+                };
+
+                db.UserRoles.Add(userRolesDTO);
+                db.SaveChanges();
+            }
+
+            // Create a TempData message
+            TempData["SM"] = "You are now registered and can login.";
+
+            // Redirect
+            return Redirect("~/account/login");
+        }
+
+        // GET: /account/Logout
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("~/account/login");
         }
     }
 }
