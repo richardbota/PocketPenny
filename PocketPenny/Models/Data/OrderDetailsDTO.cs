d2093c8e77e6b82decfb20f1a47be69363de24fc
admin <richard.bota@khk.ee>
2017-04-04 22:47:59 +0300

    Added order DTO-s

---
+++ PocketPenny/Models/Data/OrderDetailsDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PocketPenny.Models.Data
+{
+    [Table("tblOrderDetails")]
+    public class OrderDetailsDTO
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        [ForeignKey("OrderId")]
+        public virtual OrderDTO Orders { get; set; }
+        [ForeignKey("UserId")]
+        public virtual UserDTO Users { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual ProductDTO Products { get; set; }
+    }
+}
